a07682ba1fe607a6e018cd6b21594a293f1b36c2
Genius1512 <silvan.genius@gmail.com>
2021-09-21 11:19:05 -0700

    Added installer

---
+++ installer/main.py
@@ -0,0 +1,14 @@
+import zipfile
+import promptlib
+import os
+
+
+prompter = promptlib.Files()
+
+print("Select the zip file")
+zip_dir = prompter.file()
+print("Select the dir you want to install")
+install_dir = prompter.dir()
+
+with zipfile.ZipFile(zip_dir, "r") as zip:
+    zip.extractall(install_dir + r"\Shooter")
