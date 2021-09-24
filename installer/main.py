1aab04569198f0e6809ff7ed75c14d571a76eee9
Genius1512 <silvan.genius@gmail.com>
2021-09-24 11:34:29 -0700

    Updated installer

--- installer/main.py
+++ installer/main.py
@@ -5,6 +5,21 @@ import os
 
 # pyinstaller --onefile -n shooter_installer_1.3 main.py
 
+is_valid = False
+y_n = ""
+while not is_valid:
+    y_n = input("Install Shooter (y/n): ").lower()
+    if y_n == "y":
+        is_valid = True
+    elif y_n == "n":
+        print("Installing cancelled")
+        quit()
+    else:
+        print("Invalid input")
+
+print("adf")
+quit()
+
 prompter = promptlib.Files()
 
 print("Select the zip file")
